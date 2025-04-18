0c7fd8e4c27f9d3103d41cdeedbffe91f5e10e22
Korolenko <korolenkoalexey84@gmail.com>
2025-04-18 21:18:52 +0300

    fourth commit

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(e) {
+    e.preventDefault();
+    const emailValue = e.target.elements.email.value.trim();
+    const passwordValue = e.target.elements.password.value.trim();
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+    if (emailValue ==="" || passwordValue === "") {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    console.log(formData);
+     e.target.reset();
+}
+
 
