0764e80af0e570dc40e403b3c990e36dcb892df2
Korolenko <korolenkoalexey84@gmail.com>
2025-04-21 12:39:01 +0300

    hw corrections

--- js/task-5.js
+++ js/task-5.js
@@ -13,7 +13,5 @@ btn.addEventListener("click", handleBtnClick);
 function handleBtnClick(e) {
   let color = getRandomHexColor();
   document.body.style.backgroundColor = color;
-  span.textContent = `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  span.textContent = color;
 }
